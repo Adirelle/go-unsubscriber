f25d5c4f62c34161a2294a75505f71e39d82adea
Adirelle <adirelle+github@gmail.com>
2019-12-17 15:00:32 +0100

    Improve logging configuration.

--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.13
 
 require (
 	github.com/emersion/go-imap v1.0.2
+	github.com/juju/ansiterm v0.0.0-20180109212912-720a0952cc2a // indirect
 	github.com/juju/loggo v0.0.0-20190526231331-6e530bcce5d8
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/lunixbochs/vtclean v1.0.0 // indirect
 	github.com/mattn/go-colorable v0.1.4
 	golang.org/x/sys v0.0.0-20190422165155-953cdadca894 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
--- main.go
+++ main.go
@@ -2,23 +2,26 @@ package main
 
 import (
 	"flag"
-	"os"
-
 	"github.com/juju/loggo"
+	"github.com/juju/loggo/loggocolor"
+	"os"
 )
 
 func main() {
-	_, _ = loggo.ReplaceDefaultWriter(loggo.NewSimpleWriter(os.Stdout, loggo.DefaultFormatter))
-	logger := loggo.GetLogger("")
-	logger.SetLogLevel(loggo.DEBUG)
-	logger.Infof("starting")
-	defer logger.Infof("bye bye")
-
-	var configFile string
-	flag.StringVar(&configFile, "config", "./unsubscriber.json", "Path of configuration file")
+	configFile := flag.String("config", "./unsubscriber.json", "Path of configuration file")
+	enableDebug := flag.Bool("debug", false, "Display debug messages")
+	quiet := flag.Bool("quiet", false, "Only display errors")
+	verbose := flag.Bool("verbose", false, "Display more messages")
+	colors := flag.Bool("colors", true, "Enable colored output")
 	flag.Parse()
 
-	config, err := LoadConfig(configFile)
+	logger := loggo.GetLogger("")
+	logger.SetLogLevel(loggo.WARNING)
+	if *colors {
+		_, _ = loggo.ReplaceDefaultWriter(loggocolor.NewColorWriter(os.Stdout))
+	}
+
+	config, err := LoadConfig(*configFile)
 	if err != nil {
 		logger.Criticalf("could not read configuration: %s", err)
 		os.Exit(2)
@@ -26,6 +29,16 @@ func main() {
 	if err := loggo.ConfigureLoggers(config.Logs); err != nil {
 		logger.Warningf("could not configure logging: %s", err)
 	}
+
+	if *enableDebug {
+		logger.SetLogLevel(loggo.DEBUG)
+	} else if *verbose {
+		logger.SetLogLevel(loggo.INFO)
+	} else if *quiet {
+		logger.SetLogLevel(loggo.ERROR)
+	}
+	logger.Infof("starting")
+	defer logger.Infof("bye bye")
 
 	reader, err := NewMailReader(config.IMAP)
 	defer func() { _ = reader.Close() }()
